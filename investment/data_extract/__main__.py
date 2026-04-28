b68c53fbdbbde7d55ee5bf053a19ad6aadd56546
Rui Xue <ruixue.fi@gmail.com>
2026-04-28 09:08:02 +0300

    #20 made excel file path as argument

--- investment/data_extract/__main__.py
+++ investment/data_extract/__main__.py
@@ -1,5 +1,10 @@
+import sys
 from investment.data_extract.nordea_holdings_extractor import extract_from_excel
 
-holdings = extract_from_excel("data/nordea_omistukset.xlsx")
+if len(sys.argv) != 2:
+    print(f"Usage: python -m investment.data_extract <excel_file>")
+    sys.exit(1)
+
+holdings = extract_from_excel(sys.argv[1])
 for h in holdings:
     print(h)
